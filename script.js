b73e62b046b7ca1ce1fdbb614818071d1b0936ed
Jessica Ferguson <jferguson1903@gmail.com>
2021-11-14 13:34:22 -0600

    started adding initial variables and added values id's

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// BillBeforeTip variable that pulls what was entered into the field
+var BillBeforeTip = $("#BillBeforeTip").val();
+
+var TipSlider = $("#TipSlider").val();
+
+var TipQuickSelect = $("#TipQuickSelect").val();
+
 // https://codepen.io/nerooc/pen/WNQJRKP - used as basis for Language selector
 $(function(){
   $('.selectpicker').selectpicker();
